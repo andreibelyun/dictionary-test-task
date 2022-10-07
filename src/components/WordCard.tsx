31a09f237fb9b225c329a36939d35199617b735b
Andrey Belyun <andreibelyun@yandex.com>
2022-10-07 08:24:49 +0300

    feat: create word component

---
+++ src/components/WordCard.tsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import { Card, CardContent, Typography, Box } from '@mui/material';
+import WordMeaning from './WordMeaning';
+import { Word } from '../types/word';
+
+type Props = {
+  word: Word;
+};
+
+function WordCard({ word }: Props) {
+  return (
+    <article>
+      <Card
+        sx={{ m: '0 auto', mb: 2, width: { xs: '100%', sm: '80%', md: '60%' } }}
+      >
+        <CardContent>
+          <Typography variant="h4" component="h4">
+            {word.word}
+          </Typography>
+
+          <Typography
+            variant="h5"
+            component="p"
+            color="text.secondary"
+            gutterBottom
+          >
+            {`(${word.phonetic})`}
+          </Typography>
+
+          {word.phonetics && (
+            <Typography variant="h6" sx={{ mb: 2 }}>
+              Phonetics:
+            </Typography>
+          )}
+
+          {word.phonetics?.length > 0 &&
+            word.phonetics.map((phonetic) => (
+              <Box
+                key={phonetic.text}
+                sx={{ width: { xs: '100%', sm: '450px' } }}
+              >
+                <Typography
+                  variant="body1"
+                  component="p"
+                  sx={{ ml: '10px', mb: '10px' }}
+                >
+                  {phonetic.text}
+                </Typography>
+                {phonetic.audio && (
+                  <audio controls autoPlay={false}>
+                    <source src={phonetic.audio} type="audio/mpeg" />
+                  </audio>
+                )}
+              </Box>
+            ))}
+
+          {word.origin && (
+            <>
+              <Typography variant="h6" sx={{ mb: 2 }}>
+                Origin:
+              </Typography>
+
+              <Typography variant="body1" color="text.secondary">
+                {word.origin}
+              </Typography>
+            </>
+          )}
+
+          {word.meanings &&
+            word?.meanings.map((meaning) => <WordMeaning meaning={meaning} />)}
+        </CardContent>
+      </Card>
+    </article>
+  );
+}
+
+export default WordCard;
