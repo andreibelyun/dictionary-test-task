26e6d90b88cff77decbe98e3c819b3309a87c19c
Andrey Belyun <andreibelyun@yandex.com>
2022-10-07 08:23:10 +0300

    feat: create word definition component

---
+++ src/components/Definition.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Typography } from '@mui/material';
+import { DefinitionItem } from '../types/word';
+
+type Props = {
+  definition: DefinitionItem;
+};
+
+function Definition({ definition }: Props) {
+  return (
+    <div>
+      {definition.definition && (
+        <Typography variant="body1">{definition.definition}</Typography>
+      )}
+      {definition.example && (
+        <Typography variant="body2">{`(For example: ${definition.example})`}</Typography>
+      )}
+      {definition.synonyms?.length > 0 && (
+        <Typography variant="body2">
+          {`Synonyms: ${definition.synonyms.join(', ')}.`}
+        </Typography>
+      )}
+      {definition.antonyms?.length > 0 && (
+        <Typography variant="body2">
+          {`Antonyms: ${definition.antonyms.join(', ')}.`}
+        </Typography>
+      )}
+    </div>
+  );
+}
+
+export default Definition;
